0d9049f9f95a65bac661d44ac097a814f735faba
think2011 <452125301.hzplay@gmail.com>
2018-12-25 03:31:11 +0800

    init

---
+++ pages/step2/step2.js
@@ -0,0 +1,23 @@
+Page({
+  data: {
+    keywords: [
+      {
+        key: 'led',
+        checked: false,
+      },
+      {
+        key: 'led222',
+        checked: false,
+      },
+      {
+        key: 'led333',
+        checked: false,
+      },
+      {
+        key: 'led444',
+        checked: false,
+      },
+    ],
+  },
+  onLoad: function() {},
+})
