6e7914fc8720501c27f74c79b5a7314f962d7bea
think2011 <452125301.hzplay@gmail.com>
2018-12-25 12:24:24 +0800

    feat: 发送成功提示

--- pages/step5/step5.js
+++ pages/step5/step5.js
@@ -2,8 +2,19 @@ Page({
   data: {},
   onLoad: function() {},
   toNext() {
-    wx.navigateBack({
-      delta: 5,
+    wx.showLoading({
+      title: '正在发送',
     })
+
+    setTimeout(function() {
+      wx.hideLoading()
+      wx.showToast({ title: '全部发送成功' })
+
+      setTimeout(() => {
+        wx.navigateBack({
+          delta: 5,
+        })
+      }, 1000)
+    }, 2000)
   },
 })
